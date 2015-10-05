c8fb42ce49e629b8ffbde9cdaff2abd4116a1fdc
Mykhailo Bohdanov <mykhailo_bohdanov@epam.com>
2015-10-05 17:56:41 +0300

    DAOandORM

--- src/com/jquantium/dao/DAO.java
+++ src/com/jquantium/dao/DAO.java
@@ -1,57 +1,51 @@
 package com.jquantium.dao;
 
-import com.jquantium.dao.instance.TableInstance;
-import com.jquantium.dao.queries.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
-//import javax.persistence.criteria.CriteriaBuilder;
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.List;
 
 /**
- * Created by Mykhailo_Bohdanov on 26/06/2015.
+ * Created by Mykhailo_Bohdanov on 02/10/2015.
  */
-@Repository
+@Service
 public class DAO {
-    private HashMap<Integer, NamedParameterJdbcTemplate> dataSources   = new HashMap<Integer, NamedParameterJdbcTemplate>();
+    private static HashMap<Integer, NamedParameterJdbcTemplate> dataMap = new HashMap<>();
+
+    private NamedParameterJdbcTemplate getJdbc() {
+        return getJdbc(0);
+    }
+    private NamedParameterJdbcTemplate getJdbc(int id) {
+        return dataMap.get(id);
+    }
 
     @Autowired
     void init(@Qualifier("mainFrame") DataSource mainFrame, @Qualifier("userFrame") DataSource userFrame) {
-        addNode(0, mainFrame);
-        addNode(1, userFrame);
-    }
-    public void addNode(int nodeId, DataSource node) {
-        dataSources.put(nodeId, new NamedParameterJdbcTemplate(node));
-    }
-    public NamedParameterJdbcTemplate getNode(final int nodeId) throws Exception {
-        if (dataSources.containsKey(nodeId))
-            return dataSources.get(nodeId);
-
-        throw new Exception();//TODO make specific exception for this event
+        addSource(0, mainFrame);
+        addSource(1, userFrame);
     }
 
-    public int insertRow(String sql) throws Exception {
-        return insertRow(sql, null, 0);
+    public void addSource(int id, DataSource source) {
+        dataMap.put(id, new NamedParameterJdbcTemplate(source));
     }
-    public int insertRow(String sql, MapSqlParameterSource map) throws Exception {
-        return insertRow(sql, map, 0);
-    }
-    public int insertRow(String sql, MapSqlParameterSource map, final int nodeId) throws Exception {
-//        if (sql == null)
-//            throw new QueryException();
 
-        KeyHolder key   = new GeneratedKeyHolder();
+    public int putRow(String sql, MapSqlParameterSource map) throws Exception {
+        return putRow(sql, map, 0);
+    }
+
+    public int putRow(String sql, MapSqlParameterSource map, int nodeId) throws Exception {
+        NamedParameterJdbcTemplate jdbc = getJdbc();
+        KeyHolder key = new GeneratedKeyHolder();
 
         try {
-            getNode(nodeId)
+            jdbc
                     .update(
                             sql,
                             map,
@@ -64,101 +58,36 @@ public class DAO {
         return key.getKey().intValue();
     }
 
-    //TODO refactoring next methods
-    public <T> T getRow(String sql, MapSqlParameterSource map, RowMapper<T> mapper) throws Exception {
-        return getRow(sql, map, mapper, 0);
-    }
-    public <T> T getRow(String sql, MapSqlParameterSource map, RowMapper<T> mapper, final int nodeId) throws Exception {
-        if (sql == null || !dataSources.containsKey(nodeId))
-            return null;//TODO must throwable DataSourceNullException
-
-        try {
-            return dataSources
-                    .get(nodeId)
-                    .queryForObject(
-                            sql,
-                            map,
-                            mapper
-                    );
-        } catch (Exception e) {
-            throw e;
-        }
+    public <E> E getRow(String sql, MapSqlParameterSource map, Class<E> eClass) throws Exception {
+        return getRow(sql, map, eClass, 0);
     }
 
-    public <T> List<T> getRowList(String sql, MapSqlParameterSource map, RowMapper<T> mapper) throws Exception {
-        return getRowList(sql, map, mapper, 0);
+    public <E> E getRow(String sql, MapSqlParameterSource map, Class<E> eClass, int nodeId) throws Exception {
+        return null;
     }
-    public <T> List<T> getRowList(String sql, MapSqlParameterSource map, RowMapper<T> mapper, final int nodeId) throws Exception {
-        if (sql == null || !dataSources.containsKey(nodeId))
-            return null;//TODO must throwable DataSourceNullException
 
-        try {
-            if (map != null)
-                return dataSources
-                        .get(nodeId)
-                        .query(
-                                sql,
-                                map,
-                                mapper
-                        );
-            else
-                return dataSources
-                        .get(nodeId)
-                        .query(
-                                sql,
-                                mapper
-                        );
-        } catch (Exception e) {
-            throw e;
-        }
+    public <E> List<E> getRowList(String sql, MapSqlParameterSource map, Class<E> eClass) throws Exception {
+        return getRowList(sql, map, eClass, 0);
+    }
+
+    public <E> List<E> getRowList(String sql, MapSqlParameterSource map, Class<E> eClass, int nodeId) throws Exception {
+        return null;
     }
 
     public void exec(String sql, MapSqlParameterSource map) throws Exception {
         exec(sql, map, 0);
     }
+
     public void exec(String sql, MapSqlParameterSource map, int nodeId) throws Exception {
-        if (sql == null || !dataSources.containsKey(nodeId))//TODO must throwable DataSourceNullException
-            return;
-
-        if (map == null)
-            map     = new MapSqlParameterSource();
-
-        try {
-            dataSources
-                    .get(nodeId)
-                    .update(
-                            sql,
-                            map
-                    );
-        } catch (Exception e) {
-            throw e;
-        }
+        return;
     }
 
-    public <T> T exec(String sql, MapSqlParameterSource map, Class<T> _class) throws Exception {
-        return exec(sql, map, _class, 0);
-    }
-    public <T> T exec(String sql, MapSqlParameterSource map, Class<T> _class, int nodeId) throws Exception {
-        if (!dataSources.containsKey(nodeId) || sql == null)
-            return null;
-
-        if (map == null)
-            map     = new MapSqlParameterSource();
-
-        try {
-            return dataSources
-                    .get(nodeId)
-                    .queryForObject(
-                            sql,
-                            map,
-                            _class
-                    );
-        } catch (Exception e) {
-            throw e;
-        }
+    public <E> E exec(String sql, MapSqlParameterSource map, Class<E> eClass) throws Exception {
+        return exec(sql, map, eClass, 0);
     }
 
-
-
+    public <E> E exec(String sql, MapSqlParameterSource map, Class<E> eClass, int nodeId) throws Exception {
+        return null;
+    }
 
 }
---
+++ src/com/jquantium/dao/ORM.java
@@ -0,0 +1,78 @@
+package com.jquantium.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by Mykhailo_Bohdanov on 26/08/2015.
+ */
+@Repository
+public class ORM {
+
+    @Autowired
+    private DAO dao;
+
+    public int insert(Object object) throws Exception {
+        return insert(object, 0);
+    }
+    public int insert(Object object, int nodeId) throws Exception {
+        return 0;
+    }
+
+    public int[] insert(Object[] objects) throws Exception {
+        return insert(objects, 0);
+    }
+    public int[] insert(Object[] objects, int nodeId) throws Exception {
+        int[] returns = new int[objects.length];
+
+        for (int i = 0; i < objects.length; i++) {
+            returns[i] = insert(objects[i], nodeId);
+        }
+
+        return returns;
+    }
+
+    public int[] insert(List<Object> elements) throws Exception {
+        return insert(elements, 0);
+    }
+    public int[] insert(List<Object> elements, int nodeId) throws Exception {
+        int[] returns = new int[elements.size()];
+
+        for (int i = 0; i < elements.size(); i++) {
+            returns[i] = insert(elements.get(i), nodeId);
+        }
+
+        return returns;
+    }
+
+    public <E> E selectById(Class<E> eClass, int id) throws Exception {
+        return selectById(eClass, id, 0);
+    }
+    public <E> E selectById(Class<E> eClass, int id, int nodeId) throws Exception {
+        return null;
+    }
+
+    public <E> List<E> selectList(Class<E> eClass) throws Exception {
+        return selectList(eClass, 0);
+    }
+    public <E> List<E> selectList(Class<E> eClass, int nodeId) throws Exception {
+        return null;
+    }
+
+    public boolean update(Object object) throws Exception {
+        return update(object, 0);
+    }
+    public boolean update(Object object, int nodeId) throws Exception {
+        return false;
+    }
+
+    public boolean delete(Object object) throws Exception {
+        return delete(object, 0);
+    }
+    public boolean delete(Object object, int nodeId) throws Exception {
+        return false;
+    }
+
+}
